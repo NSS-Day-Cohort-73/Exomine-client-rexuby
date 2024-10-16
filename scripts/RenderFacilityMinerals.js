bb8baf94eb66389d39a125a97b33c78f1836a038
Kenny Panyavong <elp.kenny@gmail.com>
2024-10-16 14:45:10 -0500

    Fixed renderFacilityMinerals to properly render Facility Minerals

--- scripts/RenderFacilityMinerals.js
+++ scripts/RenderFacilityMinerals.js
@@ -13,13 +13,19 @@ export const renderFacilityMinerals = async (facilityId) => {
             if (selectedFacilityMinerals.length === 0) {
                 facilityMineralsHTML += "<p>No minerals available for this facility.</p>"
             } else {
-                const divStringArray = selectedFacilityMinerals.map(({ mineralId, quantity, facility, mineral }) => {
+
+                const facilityName = selectedFacilityMinerals[0].facility.name
+                
+                facilityMineralsHTML +=`<h2>Facility Minerals for ${facilityName}</h2>`
+                
+                const divStringArray = selectedFacilityMinerals.map(({ mineralId, quantity, mineral }) => {
                     return `
                         <div>
                             <input type="radio" name="facilityMineral" value="${mineralId}"/> 
-                            ${facility.name}: ${quantity} tons of ${mineral.name}
-                        </div>`;
-                });
+                             ${quantity} tons of ${mineral.name}
+                        </div>
+                        `
+                })
 
                 facilityMineralsHTML += divStringArray.join("")
             }
@@ -36,53 +42,3 @@ export const renderFacilityMinerals = async (facilityId) => {
 
     return facilityMineralsHTML;
 }
-// export const renderFacilityMinerals = async (value) => {
-//     let facilityMineralsHTML = ""
-
-//     if (!value) {
-//         facilityMineralsHTML += '<h2>Facility Minerals</h2>'
-//     } else {
-//         try {
-//             const facilityMineralsResponse = await fetch("http://localhost:8088/facilityMinerals?_expand=facility&_expand=mineral")
-//             const facilityMinerals = await facilityMineralsResponse.json()
-
-//             const activeFacilities = facilities
-//                 .filter(facility => facility.active)
-//                 .map(facility => facility.id)
-
-//             const activeFacilityMinerals = facilityMinerals.filter(activeMinerals =>
-//                 activeFacilities.includes(activeMinerals.facilityId)
-//             )
-
-//             const divStringArray = activeFacilityMinerals.map(
-//                 (facilityMineral) => {
-//                     return `
-//                             <div>
-//                                 <input type="radio" name="facilityMineral" value="${facilityMineral.mineralId}"/> 
-//                                 "${facility.name} ${facilityMineral.quantity} tons of (${mineralName})"
-//                             </div>`
-//                 })
-
-
-
-
-//             const divStringArray = facilityMinerals.map(
-//                 (facilityMineral) => {
-//                     return `<div>
-//                                 <input type="radio" name="facilityMineral" value="${facilityMineral.mineralId}"/> ${facilityMineral.mineralId}
-//                             </div>
-//                             `
-//                 }
-//             )
-//             facilityMineralsHTML += divStringArray.join("")
-//         } catch (error) {
-//         console.error('Error fetching facility minerals:', error)
-//     }
-//     const container = document.getElementById("facility-minerals-container")
-//     container.innerHTML = facilityMineralsHTML
-
-//     return facilityMineralsHTML
-//     }
-// }
-
-// Render the minerals available in the selected facility
