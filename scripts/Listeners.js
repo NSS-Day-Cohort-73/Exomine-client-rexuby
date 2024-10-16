5edd50b042322c18407c8690208b1ddd740d021c
herringvoices <nalherring@gmail.com>
2024-10-16 13:16:22 -0500

    Made the functions asyncronous and fixed naming errors

--- scripts/Listeners.js
+++ scripts/Listeners.js
@@ -1,34 +1,31 @@
 import { renderColonyMinerals } from './RenderColonyMinerals.js'
-import { renderFacilityMinerals } from './RenderFacilityMinerals.js'
-import { renderSpaceCart } from './RenderSpaceCart.js'
-import { changeState, purchaseMinerals } from './TransientState.js'
+import { renderFacilityMinerals } from './RenderFacilitiyMinerals.js'
+import { renderSpaceCart } from './SpaceCart.js'
+import { changeState, purchaseMinerals, transientState } from './TransientState.js'
 
-export const addListeners =() => {
- document.addEventListener('change', (event) =>{
-    const eventValue = event.target.eventValue
-    const type = event.target.dataset.type
+export const addListeners = () => {
+    document.addEventListener('change', async (event) => {
+        const eventValue = event.target.value
+        const type = event.target.dataset.type
 
-    if (type === 'governor'){
-        changeState('governorId', eventValue)
-        renderColonyMinerals(eventValue)
-    }else if (type === 'facility'){
-        changeState('facilityMineralsId', 0)
-        changeState('facilityId', eventValue)
-        renderFacilityMinerals(eventValue)
-        renderSpaceCart()
-    }
- })
-
- document.addEventListener('click', (event)=>{
-    if (event.target.id ==='purchase-mineral-button'){
-        if(Object.values(transientState).some(value => !value)){
-            window.alert("You don't have any items in your cart!")
-        } else{
-            purchaseMinerals()
+        if (type === 'governor') {
+            await changeState('governorId', eventValue)
+            await renderColonyMinerals(eventValue)
+        } else if (type === 'facility') {
+            await changeState('facilityMineralsId', 0)
+            await changeState('facilityId', eventValue)
+            await renderFacilityMinerals(eventValue)
+            await renderSpaceCart()
         }
-    }
+    })
 
- })
-
-
+    document.addEventListener('click', async (event) => {
+        if (event.target.id === 'purchase-mineral-button') {
+            if (Object.values(transientState).some(value => !value)) {
+                window.alert("You don't have any items in your cart!")
+            } else {
+                await purchaseMinerals()
+            }
+        }
+    })
 }
